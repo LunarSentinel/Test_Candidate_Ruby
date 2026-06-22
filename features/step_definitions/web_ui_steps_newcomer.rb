718ad81cfccc06722f23805b1efb78123a000b4a
megamind <opivash@gmail.com>
2026-06-22 14:04:40 +0000

    MicroFinal. Mozhno Check

--- features/step_definitions/web_ui_steps_newcomer.rb
+++ features/step_definitions/web_ui_steps_newcomer.rb
@@ -1,47 +1,50 @@
 # frozen_string_literal: true
 
-When(/^захожу на страницу "(.+?)"$/) do |url|
+Тогда(/^захожу на страницу "(.+?)"$/) do |url|
   visit url
   $logger.info("Страница #{url} открыта")
   sleep 1
 end
 
-Когда(/^кликаю по кнопке "([^"]*)" в середине экрана$/) do |button_text|
-  link_first = find("//a[@href='/ru/downloads/']")
+Тогда(/^кликаю по кнопке "([^"]*)" в середине экрана$/) do |button_text|
+  link_first = find(:xpath,"//a[@href='/ru/downloads/']")
   link_first.click
   $logger.info("Переход на страницу \"#{button_text}\" осуществлен")
   sleep 1
 end
 
-Когда(/^скачиваю из блока "([^"]*)" последнюю версию "([^"]*)"$/) do |stable_release, ruby_version|
-  link_first = find("//ul[2]/li[1]/ul/li[1]/a")
+Тогда(/^скачиваю из блока "([^"]*)" последнюю версию "([^"]*)"$/) do |stable_release, ruby_version|
+  link_first = find(:xpath, "//ul[2]/li[1]/ul/li[1]/a")
   link_first.click
   $logger.info("Последняя версия Ruby \"#{ruby_version}\" из блока \"#{stable_release}\" загружена")
 end
 
-Когда(/^проверяю, что файл находится в нужной директории$/) do
+Тогда(/^проверяю, что файл находится в нужной директории$/) do
   if !Dir.children("/home/pozhiloyuser/Downloads_Chrome_Firefox/Chrome").nil?
     $logger.info("Файл находится по заявленному пути #{Dir.children("/home/pozhiloyuser/Downloads_Chrome_Firefox/Chrome").last}")
+  elsif !Dir.children("/home/pozhiloyuser/Downloads_Chrome_Firefox/Firefox").nil?
+    $logger.info("Файл находится по заявленному пути #{Dir.children("/home/pozhiloyuser/Downloads_Chrome_Firefox/Firefox").last}")
   else $logger.info("Файл отсутствует по заявленному пути")
   end
 end
  
-Когда(/^проверяю, что имя скачанного файла совпадает с именем файла-установщика, указанного на сайте$/) do
-  link_first = find("//ul[2]/li[1]/ul/li[1]/a")
+Тогда(/^проверяю, что имя скачанного файла совпадает с именем файла-установщика, указанного на сайте$/) do
+  link_first = find(:xpath,"//ul[2]/li[1]/ul/li[1]/a")
   link_href = link_first[:href]
   link_file_name = File.basename(link_href)
-  $logger.info(link_file_name+'.crdownload')
-  $logger.info(Dir.children("/home/pozhiloyuser/Downloads_Chrome_Firefox/Chrome").last)
   if
   link_file_name+'.crdownload'==Dir.children("/home/pozhiloyuser/Downloads_Chrome_Firefox/Chrome").last
   $logger.info("Файл находится в нужной директории")
+  elsif
+    link_file_name+'.crdownload'==Dir.children("/home/pozhiloyuser/Downloads_Chrome_Firefox/Firefox").last
+    $logger.info("Файл находится в нужной директории")
   
   else
     $logger.error("Файл отсутствует в нужной директории")
   end
 end
 
-When(/^я должен увидеть текст на странице "([^"]*)"$/) do |text_oleg|
+Тогда(/^я должен увидеть текст на странице "([^"]*)"$/) do |text_oleg|
   sleep 1
   $logger.info("#{text_oleg}")
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -8,12 +8,15 @@ require 'capybara/cucumber'
 require 'selenium-webdriver'
 require_relative 'helpers/class_extentions'
 
+ENV['DISPLAY'] = ':0'
+ENV['MOZ_ENABLE_WAYLAND'] = '0'
+
 def browser_setup(browser = 'firefox')
   case browser
   when 'chrome'
     Capybara.register_driver :chrome do |app|
-      service = Selenium::WebDriver::Service.chrome
-      service.executable_path = '//Test_Candidate_Ruby/configuration/chromedriver'
+      executable_path =File.expand_path('configuration/chromedriver', Dir.pwd)
+      service = Selenium::WebDriver::Service.chrome(path: executable_path)
       options = Selenium::WebDriver::Chrome::Options.new
       options.add_preference('profile.default_content_settings.popups', 0)
       options.add_preference('plugins.always_open_pdf_externally', true)
@@ -22,7 +25,7 @@ def browser_setup(browser = 'firefox')
       options.add_preference(:download, 
       prompt_for_download: false, 
       default_directory: download_path)
-      Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+      Capybara::Selenium::Driver.new(app, browser: :chrome, service:service, options: options)
 
     end
     Capybara.default_driver = :chrome
@@ -30,20 +33,27 @@ def browser_setup(browser = 'firefox')
     Capybara.default_selector = :xpath
     Capybara.default_max_wait_time = 15
   else
+    
     Capybara.register_driver :firefox_driver do |app|
-      service = Selenium::WebDriver::Service.firefox
-      service.executable_path = '//Test_Candidate_Ruby/configuration/geckodriver'
+      executable_path = File.expand_path('configuration/geckodriver', Dir.pwd)
+      service = Selenium::WebDriver::Service.firefox(path: executable_path)
       options = Selenium::WebDriver::Firefox::Options.new
+      options.binary = '/usr/bin/firefox'
+
+     # options.add_argument('-headless')
+      options.add_argument('--no-sandbox')
+      options.add_argument('--disable-dev-shm-usage')
+
       options.add_preference('browser.download.folderList', 2)
-      options.add_preference('browser.download.dir', File.expand_path('./features/tmp'))
+      options.add_preference('browser.download.dir', File.expand_path('/home/pozhiloyuser/Downloads_Chrome_Firefox/Firefox'))
       options.add_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream, text/xml')
       options.add_preference('pdfjs.disabled', true)
-      Capybara::Selenium::Driver.new(app, browser: :firefox, profile: profile, port: Random.rand(7000..7999))
+      Capybara::Selenium::Driver.new(app, browser: :firefox, service: service, options: options)
     end
     Capybara.default_driver = :firefox_driver
   end
 end
-browser_setup('chrome')
+browser_setup('firefox')
 
 configuration = YAML.load_file 'configuration/default.yml'
 $rest_wrap = RestWrapper.new url: 'https://testing4qa.ediweb.ru/api',
